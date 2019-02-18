e69747920d91d0e5254d1e89da46e707a4a4971f
DeveloperDante <n3o735@gmail.com>
2019-02-19 00:08:16 +0530

    add core module and shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,6 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    SharedModule,
+    CoreModule,
     AppRoutingModule
   ],
   providers: [],
