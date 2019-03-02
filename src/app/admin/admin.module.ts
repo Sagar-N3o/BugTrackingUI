2556e1230df5b4934797e05b7f87e9b8b3c70cb0
DeveloperDante <n3o735@gmail.com>
2019-03-02 23:03:18 +0530

    add router links

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,21 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing/admin-routing.module';
-
 import { UserModule } from '../user/user.module';
+import { SharedModule } from '../shared/shared.module';
+
 import { AdminNavbarComponent } from './components/admin-navbar/admin-navbar.component';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { AdminSideNavbarComponent } from './components/admin-side-navbar/admin-side-navbar.component';
 import { AdminSystemOverviewComponent } from './components/admin-system-overview/admin-system-overview.component';
+import { AdminBugComponent } from './components/admin-bug/admin-bug.component';
 
 @NgModule({
   declarations: [
   AdminNavbarComponent,
   AdminDashboardComponent,
   AdminSideNavbarComponent,
-  AdminSystemOverviewComponent
+  AdminSystemOverviewComponent,
+  AdminBugComponent
 ],
   imports: [
+    SharedModule,
     AdminRoutingModule,
     UserModule,
     CommonModule
---
+++ src/app/admin/components/admin-bug/admin-bug.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-bug',
+  templateUrl: './admin-bug.component.html',
+  styleUrls: ['./admin-bug.component.css']
+})
+export class AdminBugComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
