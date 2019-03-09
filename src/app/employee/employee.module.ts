6a936e4b34daa36dd9af123366b21bff4cff6f09
DeveloperDante <n3o735@gmail.com>
2019-03-09 14:47:48 +0530

    work on create employee

--- src/app/employee/employee.module.ts
+++ src/app/employee/employee.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListEmployeeComponent } from './components/list-employee/list-employee.component';
+import { CreateEmployeeComponent } from './components/create-employee/create-employee.component';
 
 @NgModule({
   declarations: [
-    ListEmployeeComponent
+    ListEmployeeComponent,
+    CreateEmployeeComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    ListEmployeeComponent
+    ListEmployeeComponent,
+    CreateEmployeeComponent
   ]
 })
 export class EmployeeModule { }
