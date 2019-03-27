13214ddf5cc62a38ee4f3e2694aaf47b17ab6101
DeveloperDante <n3o735@gmail.com>
2019-03-27 21:21:19 +0530

    employee details page working

--- src/app/employee/components/employee-details/employee-details.component.ts
+++ src/app/employee/components/employee-details/employee-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from 'src/app/shared/services/employee.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee-details',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeDetailsComponent implements OnInit {
 
-  constructor() { }
+  employee = {};
+  Id: number = 0;
+  hasError: boolean = false;
+
+  constructor(
+    private _employeeService: EmployeeService,
+    private _router: Router
+  ) { }
 
   ngOnInit() {
+    this.Id = Number.parseInt(this._router.url.split('/')[4]);
+    this._employeeService.EmployeeDetails(this.Id).subscribe(
+      res => {
+        if(res['Success']) {
+          console.log(res['Data']);
+          this.employee = res['Data'];
+        }
+        else
+          this.hasError = true;
+      },
+      err => this.hasError = true
+    );
   }
 
 }
