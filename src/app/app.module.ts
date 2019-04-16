14c2ef363ea00cce5085f009cc59eef9e181fcd4
DeveloperDante <n3o735@gmail.com>
2019-04-16 17:40:30 +0530

    create employee dashboard module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,16 @@ import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 import { AdminModule } from './admin/admin.module';
 import { AppRoutingModule } from './app-routing.module';
+import { EmployeeModule } from './employee/employee.module';
+import { ProjectModule } from './project/project.module';
+import { BugModule } from './bug/bug.module';
+import { EmployeeDashboardModule } from './employee-dashboard/employee-dashboard.module';
 
 import { DashboardService } from './shared/services/dashboard.service';
 import { EmployeeService } from './shared/services/employee.service';
 import { ProjectService } from './shared/services/project.service';
 
 import { AppComponent } from './app.component';
-import { EmployeeModule } from './employee/employee.module';
-import { ProjectModule } from './project/project.module';
-import { BugModule } from './bug/bug.module';
-import { BugRoutingModule } from './bug/bug-routing/bug-routing.module';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
@@ -31,7 +31,7 @@ import { LoginComponent } from './login/login.component';
     ProjectModule,
     EmployeeModule,
     BugModule,
-    BugRoutingModule
+    EmployeeDashboardModule
   ],
   providers: [
     DashboardService,
