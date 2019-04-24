d1ab9b52df57efa505ecdd1744e164e6c93694a8
DeveloperDante <n3o735@gmail.com>
2019-04-24 11:24:29 +0530

    refector employee-profile page

---
+++ src/app/employee-dashboard/components/employee-personal-details/employee-personal-details.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { EmployeeService } from 'src/app/shared/services/employee.service';
+
+@Component({
+  selector: 'app-employee-personal-details',
+  templateUrl: './employee-personal-details.component.html',
+  styleUrls: ['./employee-personal-details.component.css']
+})
+export class EmployeePersonalDetailsComponent implements OnInit {
+
+  hasError: boolean = false;
+  Id: number = 0;
+  employee: any = {};
+
+  constructor(
+    private _employeeService: EmployeeService
+  ) { }
+
+  ngOnInit() {
+    this.Id = Number.parseInt(sessionStorage.getItem("employee_id"));
+    this._employeeService.EmployeeDetails(this.Id).subscribe(
+      res => {
+        if (res['Success'])
+          this.employee = {
+            'name': res['Data'].FirstName + " " + res['Data'].LastName,
+            'email': res['Data'].Email,
+            'address': res['Data'].Address,
+            'contectNumber': res['Data'].ContectNumber,
+            'role': res['Data'].User_RolesViewModel.RoleName,
+            'dob': res['Data'].BirthDate,
+            'experience': res['Data'].Experience
+          };
+        else
+          this.hasError = true;
+      },
+      err => this.hasError = true
+    );
+
+  }
+
+}
--- src/app/employee-dashboard/employee-dashboard.module.ts
+++ src/app/employee-dashboard/employee-dashboard.module.ts
@@ -13,6 +13,7 @@ import { EmployeeProfileComponent } from './components/employee-profile/employee
 import { EmployeeProfileEditComponent } from './components/employee-profile-edit/employee-profile-edit.component';
 import { EmployeeChangePasswordComponent } from './components/employee-change-password/employee-change-password.component';
 import { EmployeeBugDetailsComponent } from './components/employee-bug-details/employee-bug-details.component';
+import { EmployeePersonalDetailsComponent } from './components/employee-personal-details/employee-personal-details.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { EmployeeBugDetailsComponent } from './components/employee-bug-details/e
     EmployeeProfileComponent,
     EmployeeProfileEditComponent,
     EmployeeChangePasswordComponent,
-    EmployeeBugDetailsComponent
+    EmployeeBugDetailsComponent,
+    EmployeePersonalDetailsComponent
   ],
   imports: [
     CommonModule,
